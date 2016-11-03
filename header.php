76f5e780db28ef2a6a17cb67b90efa44074a47cf
JT Bicket <Bicket@users.noreply.github.com>
2016-11-02 20:26:12 -0400

    updated prospect sponsorship link

--- header.php
+++ header.php
@@ -66,7 +66,7 @@
                         <ul class="list-inline top-dark-right">                      
                             <li><a href="mailto:publicrelations@alpharangerselite.com" class="hidden-sm hidden-xs"><i class="fa fa-envelope"></i>publicrelations@alpharangerselite.com</a></li>
                 
-                            <li><a href="https://forms.zohopublic.com/virtualoffice3830/form/MemberFeedback/formperma/b1F4F4B6jk1MA75FjCe3JHaBh" target="_blank" ><i class="fa fa-user-plus"></i>Member Sponsorship</a></li>
+                            <li><a href="https://goo.gl/forms/RMgsm8O1hTt9ou2w1" target="_blank" ><i class="fa fa-user-plus"></i>Prospect Sponsorship</a></li>
                             <li><a href="http://battlefield-clans.com/index.php?a=in&u=SirBicket" target="_blank" ><i class="fa fa-check"></i> Vote for Us</a></li>
                             
                         </ul>
